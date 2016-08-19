afd368b9d5fd9af3db94552b81535d600489833c
Michael Henretty <michael.henretty@gmail.com>
2016-08-19 22:29:44 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+
+  var p = document.createElement('p');
+  p.textContent = 'hello world!';
+  document.body.appendChild(p);
+});
+
